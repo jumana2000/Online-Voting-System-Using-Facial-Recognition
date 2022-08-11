fb73d51f8e55db648f61a44bac542c190754195c
Jumana <jahanashaji2020@gmail.com>
2022-08-11 16:01:53 +0530

    User index.html template added

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.admin_index,name='admin_index'),
+    path('admin_index',views.admin_index,name='admin_index'),
     path('candidate_register',views.candidate_register,name='candidate_register'),
     path('candidate_register_data',views.candidate_register_data,name='candidate_register_data'),
     path('available_candidates',views.available_candidates,name='available_candidates'),
--- OnlineVotingSystem/urls.py
+++ OnlineVotingSystem/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Dashboard.urls'))
+    path('',include('Dashboard.urls')),
+    path('',include('VoterApp.urls'))
 ]
 
 
